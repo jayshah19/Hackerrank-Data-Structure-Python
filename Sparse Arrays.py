a9e9bc044c78e19aaa0c2a004d2b59ba4cf9a1b4
Jay Shah <97860768+jayshah19@users.noreply.github.com>
2022-11-26 12:08:37 -0500

    Add files via upload

---
+++ Sparse Arrays.py
@@ -0,0 +1,53 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+
+#
+# Complete the 'matchingStrings' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+# 1. STRING_ARRAY stringList
+# 2. STRING_ARRAY queries
+#
+
+def matchingStrings(stringList, queries):
+
+s = Counter(stringList)
+result = []
+
+for q in queries:
+result.append(s[q])
+return result
+
+
+if __name__ == '__main__':
+fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+stringList_count = int(input().strip())
+
+stringList = []
+
+for _ in range(stringList_count):
+stringList_item = input()
+stringList.append(stringList_item)
+
+queries_count = int(input().strip())
+
+queries = []
+
+for _ in range(queries_count):
+queries_item = input()
+queries.append(queries_item)
+
+res = matchingStrings(stringList, queries)
+
+fptr.write('\n'.join(map(str, res)))
+fptr.write('\n')
+
+fptr.close()
